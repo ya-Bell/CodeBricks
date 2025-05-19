9ec9d24fefeeeb5d04e6669b0513699e5c418a24
Slava <slavapertsev1337@gmail.com>
2025-05-19 08:54:33 +0700

    feat(ui): add workspace section with process control and layout
    - Implemented workspace section with a header and a button to toggle process.
    - Added a dynamic container that adjusts its size based on screen dimensions.
    - Included a delete icon and a button to start/stop the process.

--- app/src/main/java/com/example/codebricks/screens/WorkScreen.kt
+++ app/src/main/java/com/example/codebricks/screens/WorkScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -21,11 +22,16 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -48,6 +54,10 @@ fun WorkScreen() {
         Spacer(modifier = Modifier.height(8.dp))
 
         ConsoleSection()
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        WorkSpaceSection()
 
         Spacer(modifier = Modifier.height(8.dp))
 
@@ -156,3 +166,131 @@ fun ConsoleSection() {
     }
 }
 
+@Composable
+fun WorkSpaceSection() {
+    val isStarted = remember { mutableStateOf(false) }
+    val processRunning = remember { mutableStateOf(false) }
+
+    var containerWidth by remember { mutableFloatStateOf(0f) }
+    var containerHeight by remember { mutableFloatStateOf(0f) }
+
+    fun toggleProcess() {
+        processRunning.value = !processRunning.value
+        isStarted.value = !isStarted.value
+    }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                .background(Color(0xFFD9D9D9))
+                .border(1.dp, Color.Gray, RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                .padding(horizontal = 8.dp, vertical = 2.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(id = R.string.workspace_label),
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterVertically)
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .border(1.dp, Color.Black, RoundedCornerShape(12.dp))
+                    .height(26.dp)
+                    .width(60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFFFFFF),
+                    contentColor = Color.Black
+                ),
+                interactionSource = remember { MutableInteractionSource() },
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.debug_button),
+                        color = Color.Black,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold,
+                        letterSpacing = 0.5.sp,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(330.dp)
+                .background(Color.White)
+                .border(1.dp, Color.Gray)
+                .onSizeChanged { size ->
+
+                    containerWidth = size.width.toFloat()
+                    containerHeight = size.height.toFloat()
+                }
+        ) {
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp))
+                .background(Color(0xFFD9D9D9))
+                .border(1.dp, Color.Gray, RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp))
+                .padding(horizontal = 8.dp, vertical = 4.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .size(26.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_delete_forever_24),
+                    contentDescription = "Trash Bin",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            Button(
+                onClick = { toggleProcess() },
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .border(1.dp, Color.Black, RoundedCornerShape(12.dp))
+                    .height(26.dp)
+                    .width(60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFFFFFF),
+                    contentColor = Color.Black
+                ),
+                interactionSource = remember { MutableInteractionSource() },
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Text(
+                    text = if (processRunning.value) stringResource(id = R.string.stop_button) else stringResource(
+                        id = R.string.run_button
+                    ),
+                    color = Color.Black,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
