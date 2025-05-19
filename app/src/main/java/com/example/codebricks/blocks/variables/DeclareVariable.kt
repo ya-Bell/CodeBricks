9bf7ae58c09555a7a1bb8d77e7864950e5384052
Slava <slavapertsev1337@gmail.com>
2025-05-19 23:23:33 +0700

    feat(ui): add support for multiple variable declaration with comma-separated values

--- app/src/main/java/com/example/codebricks/blocks/variables/DeclareVariable.kt
+++ app/src/main/java/com/example/codebricks/blocks/variables/DeclareVariable.kt
@@ -1,9 +1,22 @@
 package com.example.codebricks.blocks.variables
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.codebricks.viewmodel.VariableViewModel
@@ -11,7 +24,8 @@ import com.example.codebricks.viewmodel.VariableViewModel
 @Composable
 fun DeclareVariable(viewModel: VariableViewModel) {
     val showDialog = remember { mutableStateOf(false) }
-    val name = remember { mutableStateOf("") }
+    val isMultiple = remember { mutableStateOf(false) }
+    val names = remember { mutableStateOf("") }
     val value = remember { mutableStateOf("") }
     val type = remember { mutableStateOf("int") }
 
@@ -29,39 +43,147 @@ fun DeclareVariable(viewModel: VariableViewModel) {
                 title = { Text("Declare Variable") },
                 text = {
                     Column {
-                        OutlinedTextField(
-                            value = name.value,
-                            onValueChange = { name.value = it },
-                            label = { Text("Name") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        OutlinedTextField(
-                            value = value.value,
-                            onValueChange = { value.value = it },
-                            label = { Text("Initial Value") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        OutlinedTextField(
-                            value = type.value,
-                            onValueChange = { type.value = it },
-                            label = { Text("Type (int, string, bool, double)") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
+                        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                            Button(
+                                onClick = { isMultiple.value = false },
+                                colors = ButtonDefaults.buttonColors(containerColor = if (!isMultiple.value) Color.Gray else Color.LightGray),
+                                modifier = Modifier.padding(4.dp).weight(1f),
+                                shape = RoundedCornerShape(10.dp)
+                            ) {
+                                Text("Single", fontWeight = FontWeight.Bold)
+                            }
+                            Button(
+                                onClick = { isMultiple.value = true },
+                                colors = ButtonDefaults.buttonColors(containerColor = if (isMultiple.value) Color.Gray else Color.LightGray),
+                                modifier = Modifier.padding(4.dp).weight(1f),
+                                shape = RoundedCornerShape(10.dp)
+                            ) {
+                                Text("Multiple", fontWeight = FontWeight.Bold)
+                            }
+                        }
+
+                        if (isMultiple.value) {
+                            OutlinedTextField(
+                                value = names.value,
+                                onValueChange = { names.value = it },
+                                label = { Text("Variable Names (comma separated)") },
+                                modifier = Modifier.fillMaxWidth(),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+
+                            OutlinedTextField(
+                                value = value.value,
+                                onValueChange = { value.value = it },
+                                label = { Text("Variable Values (comma separated)") },
+                                modifier = Modifier.fillMaxWidth(),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                        } else {
+                            OutlinedTextField(
+                                value = names.value,
+                                onValueChange = { names.value = it },
+                                label = { Text("Name") },
+                                modifier = Modifier.fillMaxWidth(),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+
+                            OutlinedTextField(
+                                value = value.value,
+                                onValueChange = { value.value = it },
+                                label = { Text("Initial Value") },
+                                modifier = Modifier.fillMaxWidth(),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                        }
+
+                        Text("Select Type", fontWeight = FontWeight.Bold)
+                        Column {
+                            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                                Button(
+                                    onClick = { type.value = "int" },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = if (type.value == "int") Color.Gray else Color.LightGray
+                                    ),
+                                    modifier = Modifier.padding(4.dp).weight(1f),
+                                    shape = RoundedCornerShape(10.dp)
+                                ) {
+                                    Text("int")
+                                }
+                                Button(
+                                    onClick = { type.value = "bool" },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = if (type.value == "bool") Color.Gray else Color.LightGray
+                                    ),
+                                    modifier = Modifier.padding(4.dp).weight(1f),
+                                    shape = RoundedCornerShape(10.dp)
+                                ) {
+                                    Text("bool")
+                                }
+                            }
+
+                            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                                Button(
+                                    onClick = { type.value = "string" },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = if (type.value == "string") Color.Gray else Color.LightGray
+                                    ),
+                                    modifier = Modifier.padding(4.dp).weight(1f),
+                                    shape = RoundedCornerShape(10.dp)
+                                ) {
+                                    Text("string")
+                                }
+                                Button(
+                                    onClick = { type.value = "double" },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = if (type.value == "double") Color.Gray else Color.LightGray
+                                    ),
+                                    modifier = Modifier.padding(4.dp).weight(1f),
+                                    shape = RoundedCornerShape(10.dp)
+                                ) {
+                                    Text("double")
+                                }
+                            }
+                        }
                     }
                 },
                 confirmButton = {
-                    Button(onClick = {
-                        val parsedValue: Any = when (type.value) {
-                            "int" -> value.value.toIntOrNull() ?: 0
-                            "bool" -> value.value.toBooleanStrictOrNull() ?: false
-                            "double" -> value.value.toDoubleOrNull() ?: 0.0
-                            else -> value.value
-                        }
+                    Button(
+                        onClick = {
+                            if (isMultiple.value) {
 
-                        viewModel.declareVariable(name.value, parsedValue, type.value)
-                        showDialog.value = false
-                    }) {
-                        Text("Create")
+                                val namesList = names.value.split(",").map { it.trim() }
+                                val valuesList = value.value.split(",").map { it.trim() }
+
+                                val valuesFinal = if (valuesList.size < namesList.size) {
+                                    valuesList + List(namesList.size - valuesList.size) { valuesList.last() }
+                                } else {
+                                    valuesList
+                                }
+
+                                namesList.forEachIndexed { index, name ->
+                                    val parsedValue: Any = when (type.value) {
+                                        "int" -> valuesFinal[index].toIntOrNull() ?: 0
+                                        "bool" -> valuesFinal[index].toBooleanStrictOrNull() == true
+                                        "double" -> valuesFinal[index].toDoubleOrNull() ?: 0.0
+                                        else -> valuesFinal[index]
+                                    }
+                                    viewModel.declareVariable(name, parsedValue, type.value)
+                                }
+                            } else {
+                                val parsedValue: Any = when (type.value) {
+                                    "int" -> value.value.toIntOrNull() ?: 0
+                                    "bool" -> value.value.toBooleanStrictOrNull() == false
+                                    "double" -> value.value.toDoubleOrNull() ?: 0.0
+                                    else -> value.value
+                                }
+                                viewModel.declareVariable(names.value, parsedValue, type.value)
+                            }
+
+                            showDialog.value = false
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4CAF50))
+                    ) {
+                        Text("Create", color = Color.White)
                     }
                 },
                 dismissButton = {
@@ -73,3 +195,5 @@ fun DeclareVariable(viewModel: VariableViewModel) {
         }
     }
 }
+
+
